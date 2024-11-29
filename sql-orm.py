6f91d37b8986a0f65dd0d43e191a2d21850a4670
Wai Yi <wongwaverley@gmail.com>
2024-11-29 13:38:52 +0000

    sql orm page setup

---
+++ sql-orm.py
@@ -0,0 +1,6 @@
+from sqlalchemy import (
+    create_engine, Column, Float, ForeignKey, Integer, String
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
